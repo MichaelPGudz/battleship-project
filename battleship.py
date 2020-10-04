a64d04fff77b21878ed62543579642dfa116f315
Jaroslaw Kaszczak <kaszczak.jaroslaw@outlook.com>
2020-10-04 20:29:38 +0200

    Start project

---
+++ battleship.py
@@ -0,0 +1,51 @@
+
+
+# INPUT
+def get_move(player):
+    pass
+
+
+def get_ai_move():
+    pass
+
+
+# OUTPUT
+def display_board(board):
+    pass
+
+
+def display_two_boards(board1, board2):
+    pass
+
+
+def display_menu():
+    pass
+
+
+def display_mode_menu():
+    pass
+
+
+# LOGIC
+def init_board(size=5):
+    pass
+
+
+def mark_move(board, sign='X'):
+    pass
+
+
+def main_menu():
+    pass
+
+
+def mode_menu():
+    pass
+
+
+def game(mode):
+    pass
+
+
+if __name__ == "__main__":
+    main_menu()
