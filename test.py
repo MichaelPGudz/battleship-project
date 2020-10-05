6c1efe8d131ec07e3397ac797b921325624870d0
Jaroslaw Kaszczak <kaszczak.jaroslaw@outlook.com>
2020-10-05 22:00:29 +0200

    Implement logo func

--- test.py
+++ test.py
@@ -10,5 +10,5 @@ board = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0,
 # battleship.display_board([[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]], pos_x=16)
 #
 # input()
-
+battleship.display_logo()
 battleship.display_two_boards(board, board)
