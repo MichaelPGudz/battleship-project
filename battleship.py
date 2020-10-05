361b10b7552f22f4ee8df197cb80e0f4d97743a3
Jaroslaw Kaszczak <kaszczak.jaroslaw@outlook.com>
2020-10-05 21:31:36 +0200

    Implement display_board func

---
+++ battleship.py
@@ -0,0 +1,123 @@
+import os, string
+from bs_cursor import Cursor
+
+
+# INPUT
+def get_move(player):
+    pass
+
+
+def get_ai_move():
+    pass
+
+
+# OUTPUT
+def display_board(board):
+    column_headers = []
+    row_headers = []
+    index = 0
+    board_size = len(board)
+    while index < board_size:
+        column_headers.append(string.ascii_uppercase[index])
+        row_headers.append(str(index + 1))
+        index += 1
+
+    print('  ' + ' '.join(column_headers))
+    index = 0
+    for row in board:
+        row_string = row_headers[index]
+        for col in row:
+            row_string += ' ' + str(col)
+        index += 1
+        print(row_string)
+
+
+def display_two_boards(board1, board2, offset=4):
+    column_headers = []
+    row_headers = []
+    index = 0
+    board_size = len(board1)
+    while index < board_size:
+        column_headers.append(string.ascii_uppercase[index])
+        row_headers.append(str(index + 1))
+        index += 1
+    # Print header
+    print(('  ' + ' '.join(column_headers) + " " * offset) * 2)
+    # Build rows
+    index = 0
+    while index < len(board1):
+        row_string = row_headers[index]
+        for col in board1[index]:
+            row_string += ' ' + str(col)
+        row_string += ' ' * offset + row_headers[index]
+        for col in board2[index]:
+            row_string += ' ' + str(col)
+        index += 1
+        # Print row
+        print(row_string)
+
+
+def display_board_with_position(board, pos_x=0, pos_y=1):
+    """WARNING! Work only in console. In PyCharm i had error."""
+    cursor = Cursor()
+    column_headers = []
+    row_headers = []
+    index = 0
+    board_size = len(board)
+    while index < board_size:
+        column_headers.append(string.ascii_uppercase[index])
+        row_headers.append(str(index + 1))
+        index += 1
+
+    cursor.print_in_position(pos_x, pos_y, '  ' + ' '.join(column_headers))
+    index = 0
+    for row in board:
+        row_string = row_headers[index]
+        for col in row:
+            row_string += ' ' + str(col)
+        index += 1
+        cursor.print_in_position(pos_x, pos_y + index, row_string)
+
+
+def display_logo():
+    pass
+
+
+def display_menu():
+    os.system("cls || clear")
+    print()
+    print(display_logo(), "\n")
+    print("Game mode: ", "\n")
+    print("MENU:\n"
+          "1 - Start Game\n"
+          "2 - Game Modes\n"
+          "3 - Exit")
+
+
+def display_mode_menu():
+    pass
+
+
+# LOGIC
+def init_board(size=5):
+    pass
+
+
+def mark_move(board, sign='X'):
+    pass
+
+
+def main_menu():
+    pass
+
+
+def mode_menu():
+    pass
+
+
+def game(mode):
+    pass
+
+
+if __name__ == "__main__":
+    main_menu()
---
+++ test.py
@@ -0,0 +1,14 @@
+import string, battleship
+import os
+
+board = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
+
+# os.system("cls || clear")
+#
+# battleship.display_board([[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]])
+#
+# battleship.display_board([[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]], pos_x=16)
+#
+# input()
+
+battleship.display_two_boards(board, board)
