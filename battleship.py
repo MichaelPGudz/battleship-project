8885ca4973763dec6cec9b867a91bc9069ec1ca5
UltraViolet5 <70701763+UltraViolet5@users.noreply.github.com>
2020-10-06 21:02:07 +0200

    place_ship(board), get_move-> get_coordinates(text)

--- battleship.py
+++ battleship.py
@@ -1,23 +1,50 @@
 from colorama import Fore, Back, Style
+get_move_text = "please select place to fire in indicated order \"row\" \"col\":  "
+palece_ship_text= "pleace select place for you ship "
 
 # INPUT
-def get_move():
+def get_coordinates(text):
     x = True
     while x:
-        move_input = input("please select place to fire in indicated order \"row\" \"col\":  ").upper()
+        move_input = input().upper(text)
         list_of_letter = list(map(chr,list(range(65,70))))
         list_of_number = list(map(str,list(range(1,6))))
         if move_input[0] not in list_of_letter:
-            print("provide correct co-ordinates")   
+            print("provide correct coordinates")   
         elif move_input[1] not in list_of_number:
-            print("provide correct co-ordinates")     
+            print("provide correct coordinates")     
         else:
-            col = move_input[1]
-            row = move_input[0]
+            row_translator = {"A": 0 , "B" : 1 , "C":2, "D": 3, "E" :4 }
+            row = row_translator[move_input[0]]
+            col = move_input[1]-1
             x= False
 
     return row, col
 
+
+def place_ship(board):
+    x = True
+    while x : 
+        x,y = get_coordinates(palece_ship_text)
+
+        if board[x][y] == "0":
+            board[x][y] = "S"
+            print(f"place {x},{y} has been taken")
+            x= False
+        else:
+            print("provide empty coordinates")
+    return board
+
+
+def is_next(board):
+     
+
+    pass
+
+
+
+    
+        
 
 def get_ai_move():
     pass
@@ -59,9 +86,11 @@ def init_board(size=5):
 #problem powienien byc zażegnany --> Style.RESET_ALL
 def mark_move(row, col, board, enemy_board):
     if enemy_board[row][col] == "0":
-        board[row][col]= Fore.BLACK + "V" + Style.RESET_ALL      
+        board[row][col]= Fore.BLACK + "V" + Style.RESET_ALL
+        enymy_board[row][col]= Fore.BLACK + "V" + Style.RESET_ALL       
     elif enemy_board[row][col] == "S":
-        board[row][col]= Fore.RED + "X" + Style.RESET_ALL    
+        board[row][col]= Fore.RED + "X" + Style.RESET_ALL 
+        enymy_board[row][col]= Fore.BLACK + "X" + Style.RESET_ALL    
     return board
 
 
